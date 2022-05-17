5e68acde9092ed0a435a933564fd8827af9d5fc2
Alexis Taniere <alexis.taniere@gmail.com>
2022-05-17 16:14:36 +0200

    Ajout du login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 
 const connection = require('../database');
 
@@ -27,7 +28,34 @@ exports.signup = (req, res, next) => {
             console.log(error);
             res.status(500).json({ error: "bcrypt malfunctionned" })
         });
+};
+
+exports.login = (req, res, next) => {
+
+    connection.query('SELECT * FROM utilisateur WHERE pseudo = ?', [req.body.pseudo], (err, result) => {
 
 
+        if (result[0] == undefined) {
+            return res.status(401).json({ erreur: "Utilisateur introuvable !" });
+        }
 
+        else {
+            bcrypt.compare(req.body.password, result[0].password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ erreur: "Mot de passe incorrect" });
+                    }
+                    res.status(200).json({
+                        userId: result[0].id,
+                        token: jwt.sign(
+                            { userId: result[0].id },
+                            "RANDOM_TOKEN_SECRET",
+                            { expiresIn: '24h' }
+                        )
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+
+        }
+    });
 };
